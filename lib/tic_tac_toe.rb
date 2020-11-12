35d40b38eb864edd77f7c730841641a09065c104
Ashley Sigler <ashleysigler13@gmail.com>
2020-11-12 20:19:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,6 +41,19 @@ def valid_move?(board, index)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 
 
 def won?(board)
