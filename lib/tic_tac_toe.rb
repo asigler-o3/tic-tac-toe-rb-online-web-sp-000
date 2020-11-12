d8e7df80ac852b599904c7ab0e6ced7ab112111c
Ashley Sigler <ashleysigler13@gmail.com>
2020-11-12 20:24:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,3 +115,23 @@ def winner(board)
     return board[won?(board)[0]]
   end
 end
+
+def play(board)
+  counter = 0
+  until counter == 9
+  turn(board)
+  counter += 1
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+  else draw?(board)
+    puts "Cat\'s Game!"
+  end
+end
